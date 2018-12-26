01af1b2063e6d4e437daa41a02b31affaf5b2894
Anton Nikifarau <411F42@gmail.com>
2018-12-27 00:32:28 +0300

    adding test file

--- src/main/java/by/bsu/Main.java
+++ src/main/java/by/bsu/Main.java
@@ -1,19 +1,55 @@
 package by.bsu;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import static java.lang.Integer.valueOf;
+import static java.util.Collections.singletonList;
+import static java.util.stream.Collectors.toList;
+
 public class Main {
 
-    private static final long P = 11L;
-    private static final long A = 4L;
-    private static final long B = 3L;
-
-    public static void main(String[] args) {
-        EllipticCurve curve = new EllipticCurve(P, A, B);
-
-        System.out.println("0 1");
-
-        for (Point point : curve.getPointsNew()) {
-            long order = curve.order(point);
-            System.out.println(point + " " + order);
+    public static void main(String[] args) throws IOException {
+        String DELIMITER = " ";
+        String testFile = "test.txt";
+        String resultFile = "result.txt";
+        if (args.length > 0) {
+            testFile = args[0];
         }
+
+        List<String> results = Files.lines(Paths.get(testFile)).map(str -> {
+            String[] values = str.split(DELIMITER);
+            if (values.length == 3) {
+                try {
+                    Integer p = valueOf(values[0]);
+                    Integer a = valueOf(values[1]);
+                    Integer b = valueOf(values[2]);
+
+                    EllipticCurve curve = new EllipticCurve(p, a, b);
+
+                    List<String> points = curve.getPointsNew().stream()
+                            .map(point -> point + " " + curve.order(point))
+                            .collect(toList());
+
+                    List<String> result = new ArrayList<>();
+                    result.add("O 1");
+                    result.addAll(points);
+                    return result;
+
+                } catch (NumberFormatException e) {
+                    return singletonList("Invalid data: " + str);
+                }
+            } else {
+                return singletonList("Invalid data: " + str);
+            }
+        }).flatMap(Collection::stream)
+                .peek(System.out::println)
+                .collect(toList());
+
+        Files.write(Paths.get(resultFile), results);
     }
 }
